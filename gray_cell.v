c97cc7dc77fa14ec2d0e93725089571f92906a89
Sudhindra2005 <154395800+Sudhindra2005@users.noreply.github.com>
2025-10-05 23:15:29 +0530

    commenetd1 gray_cell.v

--- gray_cell.v
+++ gray_cell.v
@@ -1,3 +1,8 @@
+// This is a gray cell which takes G(i:k), P(i:k) & G(k-1:j)
+// and outputs the cumulative generate G(i:j) using,
+// G(i:j) = G(i:k) + P(i:k)*G(k-1:j)
+
+// This is a purely combinational module
 module gray_cell(G21, P21, G10, G20);
     input G21, P21, G10;
     output G20;
