7dfb8fb85c18f7c4fb38061278d077c66d7447db
Sudhindra2005 <154395800+Sudhindra2005@users.noreply.github.com>
2025-10-05 23:14:24 +0530

    commented1 black_cell.v

--- black_cell.v
+++ black_cell.v
@@ -1,3 +1,9 @@
+// This is a black cell which takes G(i:k), P(i:k) & G(k-1:j), P(k-1:j)
+// and outputs the cumulative generate G(i:j) & propagate P(i:j) using,
+// G(i:j) = G(i:k) + P(i:k)*G(k-1:j)
+// P(i:j) = P(i:k)*P(k-1:j)
+
+// This is a purely combinational module
 module black_cell(G21, P21, G10, P10, G20, P20);
     input G21, P21, G10, P10;
     output G20, P20;
