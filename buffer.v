c23e7e4f5d5861e0aaed4ca6d663cc072ab487b4
Sudhindra2005 <154395800+Sudhindra2005@users.noreply.github.com>
2025-10-05 23:43:32 +0530

    commented1 buffer.v

--- buffer.v
+++ buffer.v
@@ -1,3 +1,9 @@
+// Because a particular signal gets used across multiple
+// layers, to decrease the Fan-out of a signal whichever 
+// signal has to be passed as it is in a layer, we pass
+// it through a buffer instead (A = Y)
+
+// This is a combinational module
 module buffer(A, Y);
     input A;
     output Y;
