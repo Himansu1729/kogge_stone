78c2d1fedb2d1f620eedc23268fa4b625220a48f
Sudhindra2005 <154395800+Sudhindra2005@users.noreply.github.com>
2025-10-06 00:24:10 +0530

    Update layer4.v

--- layer4.v
+++ layer4.v
@@ -1,3 +1,11 @@
+// Comes after layer 2 and contains 4 gray cells
+
+// Takes 16 G inputs from 0 to 15 and outputs also
+// 16 G at indices 0 to 15
+
+// Takes 13 P inputs from indices 3 to 15 and outputs 
+// 9 P at indices 7 to 15
+
 module layer4(P, G, CIN, OUTP4, OUTG4, CIN4);
     input [15:0] G;
     input [15:3] P;
@@ -6,13 +14,13 @@ module layer4(P, G, CIN, OUTP4, OUTG4, CIN4);
     output [15:0] OUTG4;
 	 output CIN4;
 
-    buffer b1(
+	buffer b1(				// CIN is passed through buffer
         .A(CIN),
         .Y(CIN4)
     );
 
     genvar k;
-    generate
+    generate				// G at indices 0 to 2 are passed through buffer
         for(k=0;k<3;k=k+1) begin : buffer_loop
             buffer inst1(
                 .A(G[k]),
@@ -21,15 +29,15 @@ module layer4(P, G, CIN, OUTP4, OUTG4, CIN4);
         end   
     endgenerate
 
-    gray_cell inst1(
-        .G21(G[3]),
+	gray_cell inst1(	  	// Index 3 is passed through gray cell
+		.G21(G[3]),			// requires CIN
         .P21(P[3]),
         .G10(CIN),
         .G20(OUTG4[3])
     );
     
-    genvar j;
-    generate
+    genvar j;			    // Indices 4 to 6 are also passed through
+    generate				// gray cell generating only G 
         for(j=4;j<7;j=j+1) begin : gray_cell_loop
             gray_cell inst(
                 .G21(G[j]),
@@ -40,8 +48,8 @@ module layer4(P, G, CIN, OUTP4, OUTG4, CIN4);
         end   
     endgenerate
 
-    genvar i; 
-    generate  
+    genvar i; 			  	// Indices 7 to 15 are passed through  
+    generate  				// black cells generating both P and G
         for(i=7;i<16;i=i+1) begin : black_cell_loop
             black_cell inst(
                 .G21(G[i]),
